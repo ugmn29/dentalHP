43ea9d12d8e8bc3d70a5b65ff85f7e47a3681ed6
福永真大 <fukunagashindai@fukunagashindainoMacBook-Air.local>
2026-01-28 22:03:02 +0900

    Add force-static export to sitemap and robots

--- fuku-dental/app/sitemap.ts
+++ fuku-dental/app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://fuku-dental.com'; // 実際のドメインに変更してください
 
