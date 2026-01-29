7814d4a79e585fb14868c1b10482b2ea7ca932c1
福永真大 <fukunagashindai@fukunagashindainoMacBook-Air.local>
2026-01-29 09:19:04 +0900

    Fix sitemap URL in robots.txt

--- fuku-dental/app/robots.ts
+++ fuku-dental/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/'],
     },
-    sitemap: 'https://sol-dental.com/sitemap.xml',
+    sitemap: 'https://fshika.com/sitemap.xml',
   };
 }
